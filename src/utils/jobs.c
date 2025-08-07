65ccfb75519f9c20bfebf4a6c3ae8b75cf1354ff
sulincix <parduscix@yandex.ru>
2025-08-07 17:01:59 +0000

    dont continue if job failed on another worker

--- src/utils/jobs.c
+++ src/utils/jobs.c
@@ -22,7 +22,10 @@ static void* worker_thread(void* arg) {
     jobs *j = jb->j;
     int i;
     for (i = jb->id; i < j->total; i+=j->parallel) {
-        if( j->jobs[i].call((void*)j->jobs[i].ctx, (void*)j->jobs[i].args) > 0){
+        if(j->failed){
+            return NULL;
+        }
+        if(j->jobs[i].call((void*)j->jobs[i].ctx, (void*)j->jobs[i].args) > 0){
             j->failed = true;
             return NULL;
         }
