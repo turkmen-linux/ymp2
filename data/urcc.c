2f7e694828469465ed02c720d80f2ccf75d4fa5c
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-03-02 13:53:40 +0000

    urcc improvements

--- data/urcc.c
+++ data/urcc.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+static char c;
+static size_t i = 0;
 void main(int argc, char **argv){
     FILE *f = fopen(argv[1], "rb");
-    char c;
-    size_t i = 0;
-    printf("unsigned char %s[] = {", argv[2]);
+    if(f == NULL){
+        return;
+    }
+    printf("/* %s => %s\n */", argv[1], argv[2]);
+    printf("unsigned char %s[] = {\n    ", argv[2]);
     while((c = getc(f)) != EOF){
         printf("%d, ", c);
         i++;
+        if(i % 20 == 0){
+            printf("\n    ");
+        }
     }
-    printf("0};\n");
-    printf("size_t %s_size = %ld;\n", argv[2], i);
+    printf("0\n};\n");
+    printf("size_t %s_size = %ld;\n\n", argv[2], i);
+    fclose(f);
 }
